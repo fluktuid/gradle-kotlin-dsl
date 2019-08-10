a488f40ef2500e5557334de44ca2b41f623a87e1
ashish kumar <ashishkujoy@gmail.com>
2019-08-10 14:30:56 +0530

    Adds docker run plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,12 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val dockerPluginVersion = "0.22.1"
+
     application
     kotlin("jvm") version "1.3.41"
-    id("com.palantir.docker") version "0.22.1"
+    id("com.palantir.docker") version dockerPluginVersion
+    id("com.palantir.docker-run") version dockerPluginVersion
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
@@ -38,9 +41,24 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val dockerImageName = "ashishkujoy/kotlin-dsl"
+
 docker {
-    name = "ashishkujoy/kotlin-dsl"
-    files(tasks.get("shadowJar").outputs)
+    name = dockerImageName
+    files(tasks["shadowJar"].outputs)
     setDockerfile(file("Dockerfile"))
     labels(mapOf("app-name" to "kotline", "app-version" to appVersion))
+    noCache(true)
+}
+
+dockerRun {
+    name = "kotlin-app"
+    image = dockerImageName
+    daemonize = false
+    env(mapOf("APP_NAME" to "Kotlin-dsl", "PLUGIN_USED" to "palantir/docker"))
+    volumes(mapOf(project.file("FooBarForDocker.txt") to "/FooBar.txt"))
+    ports("3000:3001","4000:4001")
+    clean = false
+    // we are not using port in our application but port are getting mapped or not can be confirmed by
+    // executing `docker container inspect kotlin-app`
 }
--- src/main/kotlin/com/learning/Application.kt
+++ src/main/kotlin/com/learning/Application.kt
@@ -1,10 +1,19 @@
 package com.learning
 
+import java.io.File
+
 object Application {
     @JvmStatic
     fun main(args: Array<String>) {
+        println("APP-NAME    -----> ${System.getenv("APP_NAME") ?: "APP_NAME is missing"}")
+        println("PLUGIN-USED -----> ${System.getenv("PLUGIN_USED") ?: "PLUGIN_USED is missing"}")
         println("Application is running")
         println("hello world")
+
+        File("FooBar.txt").useLines { fooBarLines ->
+            fooBarLines.forEach { println(it) }
+        }
+
         println("bye bye world")
         println("Application shutting down")
     }
